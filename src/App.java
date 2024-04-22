a75b14b9846c992a2fe3504d23794c0381ced316
Anna Tiberg <anastasia-elisabeth.tiberg@elev.ga.ntig.se>
2024-04-22 09:37:43 +0200

    .

--- src/App.java
+++ src/App.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class App {
     private static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args, String[] personNamn) throws Exception {
+    public static void main(String[] argsStrings) throws Exception {
        // double prisVuxen = 299.90;
         int antalPersoner = 19;
         int[] födelsedatum = new int[antalPersoner];
-        String[] personNamn = new String[antalPersoner];  
+      //  String[] personNamn = new String[antalPersoner];  
         menyVal();
         int alternativ = Integer.parseInt(scanner.nextLine());
-        while(alternativ!=4){
+        while(alternativ!=5){
             switch (alternativ) {
                 case 1:
-                    födelsedatum = bokaPlats(födelsedatum, personNamn);
+                    födelsedatum = bokaPlats(födelsedatum);
                     break;
                 case 2:
                     ledigaPlatser(födelsedatum);
@@ -19,6 +19,7 @@ public class App {
                 case 3:
                    // vinster(prisVuxen);
                     break;
+                case 4:
 
                 default:
                     break;
@@ -33,9 +34,10 @@ public class App {
         System.out.println("1. Boka en obokad plats");
         System.out.println("2. Visa bokade platser");
         System.out.println("3. Beräkna vinster");
-        System.out.println("4. Avsluta.");
+        System.out.println("4. Hitta bokning");
+        System.out.println("5. Avsluta.");
     }
-    public static int[] bokaPlats(int[] födelsedatum, String[] personNamn){
+    public static int[] bokaPlats(int[] födelsedatum){
         
         System.out.println("Välj sittplats nummer för bussresa. Plattser 1-20.");
         int sittplats = Integer.parseInt(scanner.nextLine());   
@@ -52,10 +54,10 @@ public class App {
                     System.out.println("Du har bokat sittpaltsen "+sittplats);
                     break;
                 }
-                System.out.println("Skriv in namn för bokning. För och Efternamn.");
-                String namn = scanner.nextLine();
-                personNamn[sittplats-1]=namn;
-                bokaPlats(födelsedatum, personNamn);
+              //  System.out.println("Skriv in namn för bokning. För och Efternamn.");
+             //   String namn = scanner.nextLine();
+                //personNamn[sittplats-1]=namn;
+                //bokaPlats(födelsedatum, personNamn);
             }else{
                 System.out.println("Sittplats "+sittplats+" finns ej, försök igen. ");
                 sittplats = Integer.parseInt(scanner.nextLine()); 
