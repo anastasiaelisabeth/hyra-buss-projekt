f51898f19073611ad84db5a97085cecb6ebd6d32
Anna Tiberg <anastasia-elisabeth.tiberg@elev.ga.ntig.se>
2024-04-25 18:57:52 +0200

    avboka, räkna vinst, hitta bokning

--- src/App.java
+++ src/App.java
@@ -3,12 +3,12 @@ public class App {
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] argsStrings) throws Exception {
        // double prisVuxen = 299.90;
-        int antalPersoner = 19;
+        int antalPersoner = 20;
         int[] födelsedatum = new int[antalPersoner];
       //  String[] personNamn = new String[antalPersoner];  
         menyVal();
         int alternativ = Integer.parseInt(scanner.nextLine());
-        while(alternativ!=5){
+        while(alternativ!=6){
             switch (alternativ) {
                 case 1:
                     födelsedatum = bokaPlats(födelsedatum);
@@ -17,10 +17,14 @@ public class App {
                     ledigaPlatser(födelsedatum);
                     break;
                 case 3:
-                   // vinster(prisVuxen);
+                    vinster(födelsedatum);
                     break;
                 case 4:
-
+                    hittaBokning(födelsedatum);
+                    break;
+                case 5:
+                    födelsedatum=avboka(födelsedatum);
+                    break;
                 default:
                     break;
             }
@@ -35,34 +39,34 @@ public class App {
         System.out.println("2. Visa bokade platser");
         System.out.println("3. Beräkna vinster");
         System.out.println("4. Hitta bokning");
-        System.out.println("5. Avsluta.");
+        System.out.println("5. Avboka bokad platts.");
+        System.out.println(". Avsluta.");
     }
     public static int[] bokaPlats(int[] födelsedatum){
-        
-        System.out.println("Välj sittplats nummer för bussresa. Plattser 1-20.");
-        int sittplats = Integer.parseInt(scanner.nextLine());   
-        while(true){
-            if (sittplats<21 && sittplats>0) {
-                System.out.println("För in födelsedatum");
-                int datum = Integer.parseInt(scanner.nextLine());
-                if (datum>20240501) {
-                    System.out.println("Detta datum finns ej, skriv in giltig födelsedatum");
-                    datum = Integer.parseInt(scanner.nextLine());
+        try {
+            System.out.println("Välj sittplats nummer för bussresa. Plattser 1-20.");
+            int sittplats = Integer.parseInt(scanner.nextLine());   
+            while(true){
+                if (sittplats<21 && sittplats>0) {
+                    System.out.println("För in födelsedatum");
+                    int datum = Integer.parseInt(scanner.nextLine());
+                    if (datum>20240501) {
+                        System.out.println("Detta datum finns ej, skriv in giltig födelsedatum");
+                        datum = Integer.parseInt(scanner.nextLine());
+                    }
+                    if(födelsedatum[sittplats-1]==0){
+                        födelsedatum[sittplats-1]=datum;
+                        System.out.println("Du har bokat sittpaltsen "+sittplats);
+                        break;
+                    }
+                }else{
+                    System.out.println("Sittplats "+sittplats+" finns ej, försök igen. ");
+                    sittplats = Integer.parseInt(scanner.nextLine()); 
                 }
-                if(födelsedatum[sittplats-1]==0){
-                    födelsedatum[sittplats-1]=datum;
-                    System.out.println("Du har bokat sittpaltsen "+sittplats);
-                    break;
-                }
-              //  System.out.println("Skriv in namn för bokning. För och Efternamn.");
-             //   String namn = scanner.nextLine();
-                //personNamn[sittplats-1]=namn;
-                //bokaPlats(födelsedatum, personNamn);
-            }else{
-                System.out.println("Sittplats "+sittplats+" finns ej, försök igen. ");
-                sittplats = Integer.parseInt(scanner.nextLine()); 
             }
-        }
+        } catch (Exception e) {
+            System.out.println("Fel datum, detta datum har ej varit");
+        }        
         return födelsedatum;
     }
     private static void ledigaPlatser(int[] födelsedatum){
@@ -71,15 +75,47 @@ public class App {
         }
 
     }
-   // private static double vinster(int vuxnabiljättSålda, int barnbiljättSålda){ //antalet vuxa biljätter sålda och antalet bar
-    //    double prisVuxen = 299.90;
-      //  double prisBarn = 199.90;
-      //  }
-  //  }
-   // public static String[] namnSparande(String[] personNamn){
-       // System.out.println("Skriv in ditt fullständiga namn");
-      //  double fulltNamn = Integer.parseInt(scanner.nextLine());
-      //  System.out.println("Du har skrivit in namnet "+fulltNamn);
-      //  return personNamn;
-    //}
+    private static void vinster(int[] födelsedatum){ //antalet vuxa biljätter sålda och antalet bar
+        double prisVuxen = 299.90;
+        double prisBarn = 149.90;
+        double summa = 0;
+        for(int i = 0; i<födelsedatum.length; i++){
+            if(födelsedatum[i]!=0){
+                if(födelsedatum[i]<20070000){
+                    summa+=prisVuxen;
+                }else{
+                    summa+=prisBarn;
+                }
+            }
+        }
+        System.out.println("Vinsten av sålda biljätter är: "+summa);
+    }
+    private static void hittaBokning(int[] födelsedatumArray){
+        try {
+            System.out.println("Ange personnummer för att hitta bokning av sittplats.");
+            int personnummer = Integer.parseInt(scanner.nextLine());  
+            for(int i = 0; i<födelsedatumArray.length;i++){
+                if(födelsedatumArray[i]==personnummer){
+                    System.out.println("Denna person har bokat platsen: "+(i+1));
+                    return;
+                }
+            }
+            System.out.println("Det finns ingen bokning under detta personnummer.");
+        } catch (Exception e) {
+            System.out.println("Fel format av datum, försök (YYYYMMDD).");
+        }
+    }
+    private static int[] avboka(int[] födelsedatum){
+        System.out.println("Ange personnumret du vill avboka.");
+        int personnummer = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < födelsedatum.length; i++) {
+            if (personnummer == födelsedatum[i]) {
+                födelsedatum[i] = 0;
+                System.out.println("Du har avbokat plats: "+(i+1) + ".");
+                return födelsedatum;
+            }
+        }
+        System.out.println("Gick inte att hitta bokning.");
+        return födelsedatum;
+    }
 }
